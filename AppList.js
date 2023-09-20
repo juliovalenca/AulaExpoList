f97df7e31015cb4ef9a3ce9464d6992a9ef95c9d
Naslaproject <naslaalves208@gmail.com>
2023-09-19 21:02:37 -0300

    adicionando meus arquivos implementados na sala de aula

--- AppList.js
+++ AppList.js
@@ -1,19 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-export default function AppLis() {
-  return (
-    <View style={styles.container}>
-      <Text>Lista!</Text>
-      <StatusBar style='light' />
-    </View>
-  );
+import { StatusBar } from "expo-status-bar"; 
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+export default function AppList(){
+    return(
+        <View style = {styles.container}>
+            <Text>Se não fosse Enrique eu estava fudido!</Text>
+            <StatusBar style="light"/>
+        </View>
+    );
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#D93600',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+     flex: 1,      
+     backgroundColor: '#D93600',
+     alignItems: 'center',
+     justifyContent:'center'  
+    } 
+ });
+
